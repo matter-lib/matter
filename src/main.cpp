c53972540b8dd7973f32ec214817809b7e0632c5
Harry Stanton <stanton@harry.city>
2019-06-16 13:09:57 +0100

    Added code license & various code style fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
+/**
+* MIT License
+* Copyright (c) 2019 Matter Team
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include <iostream>
 #include <SDL2/SDL.h>
 
@@ -5,49 +27,49 @@
 
 int main()
 {
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    fprintf(stderr, "SLD init error: %s\n", SDL_GetError());
-    return 1;
-  }
-
-  SDL_Window *window = SDL_CreateWindow("app", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, (SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE));
-
-  if (window == NULL) {
-    printf("Could not create window: %s", SDL_GetError());
-    return 1;
-  }
-
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  Window win = Window(window, renderer);
-
-  Control testControl = Control();
-  testControl.allowsClick = true;
-  win.setRootControl(&testControl);
-
-  bool quit = false;
-  while (!quit)
-  {
-    SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
-      win.processEvents(&event);
-
-      if (event.type == SDL_QUIT) {
-        quit = true;
-      }
-
-      // Render other stuff here
-      SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-      SDL_RenderClear(renderer);
-
-      win.render();
-
-      SDL_RenderPresent(renderer);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "SLD init error: %s\n", SDL_GetError());
+        return 1;
     }
-  }
+
+    SDL_Window *window = SDL_CreateWindow("matter", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, (SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE));
+
+    if (window == NULL) {
+        printf("Could not create window: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    Window win = Window(window, renderer);
+
+    Control testControl = Control();
+    testControl.allowsClick = true;
+    win.setRootControl(&testControl);
+
+    bool quit = false;
+    while (!quit)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            win.processEvents(&event);
+
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+
+            // Render other stuff here
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderClear(renderer);
+
+            win.render();
+
+            SDL_RenderPresent(renderer);
+        }
+    }
 
 
-  SDL_Quit();
+    SDL_Quit();
 
-  return 0;
+    return 0;
 }
